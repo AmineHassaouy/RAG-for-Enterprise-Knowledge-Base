197a6609101c9e5eaa2b992142dc842fd6524679
Amine Hassaouy <a.hassaouy@gmail.com>
2026-09-18 21:16:41 +0100

    .....

--- generation/answer_generator.py
+++ generation/answer_generator.py
@@ -1,29 +1,48 @@
 import os
+
 from mistralai.client import Mistral
+
 from indexing.vector_store import SearchResult
 from generation.prompt import build_prompt
 
+
 class AnswerGenerator:
 
-    def __init__(self, model: str = "mistral-small-lattest"):
+    def __init__(
+        self,
+        model: str = "mistral-small-latest",
+    ):
         api_key = os.getenv("MISTRAL_API_KEY")
 
         if not api_key:
-            raise ValueError("MISTRAL_API_KEY isnt set!")
+            raise ValueError(
+                "MISTRAL_API_KEY environment variable is not set."
+            )
 
         self.client = Mistral(api_key=api_key)
         self.model = model
 
-    def generate(self, query: str, results: list[SearchResult]) -> str:
+    def generate(
+        self,
+        query: str,
+        results: list[SearchResult],
+    ) -> str:
+
         if not query.strip():
             return ""
 
-        prompt = build_prompt(query=query, results=results)
+        prompt = build_prompt(
+            query=query,
+            results=results,
+        )
 
         response = self.client.chat.complete(
             model=self.model,
             messages=[
-                {"role": "user", "content": prompt,}
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
             ],
         )
 
